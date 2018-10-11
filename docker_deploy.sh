36001e08dd59a79291670219756ae72a690bb51f
Alexandra DeWit <alexa.dewit@gmail.com>
2018-10-11 16:27:14 +0200

    Add travis file

---
+++ docker_deploy.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+echo "$DOCKER_PASSWORD" | docker login --username $DOCKER_USERNAME --password-stdin
+if [ "$TRAVIS_PULL_REQUEST" = "false" ]
+then
+    export DOCKER_BRANCH_TAG=$TRAVIS_BRANCH
+else
+    export DOCKER_BRANCH_TAG=$TRAVIS_PULL_REQUEST_BRANCH
+fi
+
+if [ "$DOCKER_BRANCH" = "master" ]
+then
+  docker build -t $DOCKER_REPOSITORY:$TRAVIS_COMMIT -t $DOCKER_REPOSITORY:$DOCKER_BRANCH_TAG -t $DOCKER_REPOSITORY:latest .
+else
+  docker build -t $DOCKER_REPOSITORY:$TRAVIS_COMMIT -t $DOCKER_REPOSITORY:$DOCKER_BRANCH_TAG .
+fi
+docker push $DOCKER_REPOSITORY
