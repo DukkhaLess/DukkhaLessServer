4e57cdcbf069b8a5699f85f540d263d1d25f1bcb
Alexandra DeWit <alexa.dewit@gmail.com>
2018-10-13 10:22:09 +0200

    Working on implementing lenses and data structures

--- app/Types.hs
+++ app/Types.hs
@@ -1,6 +1,8 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Types where
 
 import           Protolude
+import           Control.Lens.Combinators
 import           Data.Aeson
 
 newtype Username = Username Text
@@ -23,17 +25,27 @@ newtype PublicKey = PublicKey Base64Content
 instance ToJSON PublicKey
 instance FromJSON PublicKey
 
-data RegisterUser
-  = User
-    { username :: Username
-    , password :: RawPassword
-    , publicKey :: PublicKey
-    }
+data RegisterUser = RegisterUser Username RawPassword PublicKey
     deriving (Generic, Eq, Show)
-instance ToJSON RegisterUser
-instance FromJSON RegisterUser
+instance FromJSON RegisterUser where
+  parseJSON = withObject "loginUser" $ \o ->
+    RegisterUser <$> o .: "username" <*> o .: "password" <*> o .: "publicKey"
 
 newtype SessionToken = SessionToken Base64Content
   deriving (Eq, Show, Generic)
 instance ToJSON SessionToken
 instance FromJSON SessionToken
+
+data LoginUser = LoginUser Username RawPassword
+  deriving (Eq, Show, Generic)
+instance FromJSON LoginUser where
+  parseJSON = withObject "loginUser" $ \o ->
+    LoginUser <$> o .: "username" <*> o .: "password"
+
+class HasRawPassword a where rawPassword :: Lens' a RawPassword
+instance HasRawPassword RawPassword where rawPassword = identity
+instance HasRawPassword LoginUser where rawPassword = lens (\(LoginUser _ p) -> p) (\(LoginUser u _) p -> LoginUser u p)
+
+class HasUsername a where username :: Lens' a Username
+instance HasUsername Username where username = identity
+instance HasUsername LoginUser where username = lens (\(LoginUser u _) -> u) (\(LoginUser _ p) u -> LoginUser u p)
