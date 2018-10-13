97b01604aa55363a7a42e888f16cacaf6af6e2e1
Alexandra DeWit <alexa.dewit@gmail.com>
2018-10-13 14:43:15 +0200

    Mocked response implemented

--- Dockerfile
+++ Dockerfile
@@ -15,5 +15,5 @@ RUN apt-get update && apt-get install -y \
   ca-certificates \
   libgmp-dev
 COPY --from=build /opt/build/.stack-work/install/x86_64-linux/lts-12.11/8.4.3/bin/dukkhaless-app /opt/app/dukkhaless-app
-EXPOSE 3000
+EXPOSE 4000
 CMD ["/opt/app/dukkhaless-app"]
--- app/App.hs
+++ app/App.hs
@@ -6,11 +6,19 @@ import           Protolude                      ( IO
                                                 , (.)
                                                 )
 import           Control.Lens
+import           Network.Wai.Middleware.RequestLogger
+                                                ( logStdoutDev )
+import           Network.Wai.Middleware.Rewrite ( PathsAndQueries
+                                                , rewritePureWithQueries
+                                                )
+import           Network.HTTP.Types.Header      ( RequestHeaders )
 import           Web.Scotty
 import           Types
 
 app :: IO ()
-app = scotty 3000 $ do
+app = scotty 4000 $ do
+  middleware $ rewritePureWithQueries removeApiPrefix
+  middleware logStdoutDev
   get "/:word" $ html "Hi"
   post "/login" $ do
     loginUser <- jsonData :: ActionM LoginUser
@@ -18,3 +26,7 @@ app = scotty 3000 $ do
   post "/register" $ do
     registerUser <- jsonData :: ActionM RegisterUser
     text $ registerUser ^. (username . _text)
+
+removeApiPrefix :: PathsAndQueries -> RequestHeaders -> PathsAndQueries
+removeApiPrefix (("api" : tail), queries) _ = (tail, queries)
+removeApiPrefix paq                       _ = paq
