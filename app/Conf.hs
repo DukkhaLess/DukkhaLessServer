6ffc4f26ca24e863734292ada791d7b61e8bdbf2
Alexandra DeWit <alexa.dewit@gmail.com>
2018-10-13 14:51:14 +0200

    Dummy conf module, needs type refinement

---
+++ app/Conf.hs
@@ -0,0 +1,41 @@
+module Conf where
+
+import           Protolude                      ( show
+                                                , flip
+                                                , (++)
+                                                , Read
+                                                , Show
+                                                , Int
+                                                , (.)
+                                                )
+import           Data.Text.Lazy                 ( toLower
+                                                , pack
+                                                , Text
+                                                )
+import           Network.Wai.Middleware.RequestLogger
+                                                ( logStdout
+                                                , logStdoutDev
+                                                )
+import           Network.Wai                    ( Middleware )
+
+data Environment
+  = Development
+  | Staging
+  | Production
+  deriving (Read, Show)
+
+logger :: Environment -> Middleware
+logger Production = logStdout
+logger _          = logStdoutDev
+
+confFileName :: Environment -> Text
+confFileName = toLower . pack . flip (++) ".conf" . show
+
+data Config
+  = Config
+    { postgresUsername :: Text
+    , postgresPassword :: Text
+    , postgresHost :: Text
+    , postgresPort :: Int
+    , postgresDatabase :: Text
+    }
