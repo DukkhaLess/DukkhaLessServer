568695ed6b7f696f5c4855660f2df6c5a2b774aa
Alexandra DeWit <alexa.dewit@gmail.com>
2018-10-13 11:23:59 +0200

    More lenses!

--- app/Types.hs
+++ app/Types.hs
@@ -8,6 +8,7 @@ import           Protolude                      ( Eq
                                                 , (<$>)
                                                 , identity
                                                 , Generic
+                                                , (.)
                                                 )
 import           Control.Lens.Combinators
 import           Data.Aeson
@@ -54,11 +55,21 @@ class HasText a where _text :: Lens' a Text
 instance HasText Text where _text = identity
 instance HasText RawPassword where _text = lens (\(RawPassword t) -> t) (\_ t -> RawPassword t)
 instance HasText Username where _text = lens (\(Username t) -> t) (\_ t -> Username t)
+instance HasText Base64Content where _text = lens (\(Base64Content t) -> t) (\_ t -> Base64Content t)
+instance HasText PublicKey where _text = base64Content . _text
+
+class HasBase64Content a where base64Content :: Lens' a Base64Content
+instance HasBase64Content Base64Content where base64Content = identity
+instance HasBase64Content PublicKey where base64Content = lens (\(PublicKey b) -> b) (\_ c -> PublicKey c)
 
 class HasRawPassword a where rawPassword :: Lens' a RawPassword
 instance HasRawPassword RawPassword where rawPassword = identity
 instance HasRawPassword LoginUser where rawPassword = lens (\(LoginUser _ p) -> p) (\(LoginUser u _) p -> LoginUser u p)
+instance HasRawPassword RegisterUser where rawPassword = lens (\(RegisterUser _ p _) -> p) (\(RegisterUser u _ k) p -> RegisterUser u p k)
 
 class HasUsername a where username :: Lens' a Username
 instance HasUsername Username where username = identity
 instance HasUsername LoginUser where username = lens (\(LoginUser u _) -> u) (\(LoginUser _ p) u -> LoginUser u p)
+instance HasUsername RegisterUser where username = lens (\(RegisterUser u _ _) -> u) (\(RegisterUser _ p k) u -> RegisterUser u p k)
+
+class HasPublicKey a where publicKey :: Lens' a PublicKey
