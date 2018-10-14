d338b42c6d43f5eeb4c89d3906f009fba978eeb9
Alexandra DeWit <alexa.dewit@gmail.com>
2018-10-14 10:21:43 +0200

    Write config building function

--- app/Conf.hs
+++ app/Conf.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Conf where
 
 import           Protolude                      ( show
@@ -7,7 +8,11 @@ import           Protolude                      ( show
                                                 , Show
                                                 , Int
                                                 , (.)
+                                                , IO
+                                                , return
+                                                , ($)
                                                 )
+import           Control.Monad.Trans.Maybe      ( MaybeT(..) )
 import           Data.Text.Lazy                 ( toLower
                                                 , pack
                                                 , Text
@@ -17,6 +22,8 @@ import           Network.Wai.Middleware.RequestLogger
                                                 , logStdoutDev
                                                 )
 import           Network.Wai                    ( Middleware )
+import qualified Data.Configurator             as C
+import qualified Data.Configurator.Types       as C
 
 data Environment
   = Development
@@ -50,3 +57,24 @@ data Config
     { databaseConfig :: DatabaseConfig
     , signingKey :: Text
     }
+
+
+makeConfig :: C.Config -> MaybeT IO Config
+makeConfig conf = do
+  dbConf <- do
+    root <- do
+      name     <- MaybeT $ C.lookup conf "postgres.root.username"
+      pass <- MaybeT $ C.lookup conf "postgres.root.password"
+      database <- MaybeT $ C.lookup conf "postgres.root.database"
+      return $ Conf.DatabaseUser name pass database
+    app <- do
+      name     <- MaybeT $ C.lookup conf "postgres.app.username"
+      pass <- MaybeT $ C.lookup conf "postgres.app.password"
+      database <- MaybeT $ C.lookup conf "postgres.app.database"
+      return $ Conf.DatabaseUser name pass database
+    hostname <- MaybeT $ C.lookup conf "postgres.host"
+    port     <- MaybeT $ C.lookup conf "postgres.port"
+    return $ Conf.DatabaseConfig root app port hostname
+  sk <- MaybeT $ C.lookup conf "crypto.signingKey"
+  return $ Conf.Config dbConf sk
+
