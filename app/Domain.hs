5b3e6f3bf29ea271a1a54b541b674ca09193f03a
Alexandra DeWit <alexa.dewit@gmail.com>
2018-11-04 20:58:05 +0100

    Access token creation function :+1:

--- app/Domain.hs
+++ app/Domain.hs
@@ -1,7 +1,9 @@
 module Domain where
 
 import           Protolude
-import           Schema
+import           Schema                         ( UserT(User)
+                                                , User
+                                                )
 import           Control.Lens
 import           Types
 import           Crypto                         ( hashPassword )
@@ -9,7 +11,10 @@ import           Data.UUID                      ( UUID )
 import           Data.Time.LocalTime            ( utcToLocalTime
                                                 , utc
                                                 )
-import           Data.Time.Clock                ( getCurrentTime )
+import           Data.Time.Clock                ( getCurrentTime
+                                                , UTCTime(..)
+                                                )
+import           Data.Time.Calendar             ( addDays )
 import           System.Random                  ( randomIO )
 import           Crypto.Argon2                  ( Argon2Status )
 
@@ -19,3 +24,13 @@ newUser (RegisterUser name rawPass pubKey) (PasswordSalt salt) = do
   hashedPass <- ExceptT $ pure $ hashPassword salt rawPass
   now        <- lift $ utcToLocalTime utc <$> getCurrentTime
   pure $ User uuid (name ^. _text) (hashedPass ^. _text) (pubKey ^. _text) now
+
+createAccessToken :: User -> IO AccessToken
+createAccessToken (User uuid _ _ _ _) = do
+  inTwoDays <- getCurrentTime
+    <&> \(UTCTime day dayTime) -> UTCTime (addDays 2 day) dayTime
+  token <- randomIO <&> TokenId
+  let uid = UserId uuid
+  pure $ AccessToken token uid (Expiry inTwoDays)
+
+
