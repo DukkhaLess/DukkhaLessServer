46a6b7fac889c0bf5d4ac8502f1bf5f6a473a6d1
Alexandra DeWit <alexa.dewit@gmail.com>
2018-11-06 16:55:17 +0100

    Removing unncessary constraints

--- app/Schema.hs
+++ app/Schema.hs
@@ -18,13 +18,9 @@ import           Database.Beam.Migrate.Types    ( CheckedDatabaseSettings
                                                 , evaluateDatabase
                                                 , unCheckDatabase
                                                 )
-import           Database.Beam.Query            ( insert
-                                                , runInsert
-                                                , insertValues
-                                                )
+import           Database.Beam.Query
 import           Database.Beam.Migrate.Simple   ( bringUpToDate )
-import           Database.Beam.Postgres         ( Postgres
-                                                , PgCommandSyntax
+import           Database.Beam.Postgres         ( PgCommandSyntax
                                                 , Connection
                                                 , Pg
                                                 )
@@ -32,14 +28,11 @@ import           Database.Beam.Postgres.Migrate ( migrationBackend )
 import           Schema.V0001            hiding ( migration )
 import qualified Schema.V0001                  as V0001
 
-dukkhalessDb :: DatabaseSettings Postgres DukkhalessDb
+dukkhalessDb :: DatabaseSettings be DukkhalessDb
 dukkhalessDb = unCheckDatabase (evaluateDatabase migrations)
 
 migrations
-  :: MigrationSteps
-       PgCommandSyntax
-       ()
-       (CheckedDatabaseSettings Postgres DukkhalessDb)
+  :: MigrationSteps PgCommandSyntax () (CheckedDatabaseSettings be DukkhalessDb)
 migrations = migrationStep "Initial commit" V0001.migration
 
 runMigrations :: Connection -> IO ()
